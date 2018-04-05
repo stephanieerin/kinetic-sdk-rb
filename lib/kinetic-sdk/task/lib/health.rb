745965f8a4bc4f83c09eb610fdf11ff54a4d52c5
Jack Boespflug <jack.boespflug@kineticdata.com>
2018-04-04 23:08:20 -0500

    Fix path when checking if server is alive

--- lib/kinetic-sdk/task/lib/health.rb
+++ lib/kinetic-sdk/task/lib/health.rb
@@ -17,6 +17,7 @@ module KineticSdk
     # @param headers [Hash] hash of headers to send, default is basic authentication
     # @return [KineticSdk::Utils::KineticHttpResponse] object, with +code+, +message+, +content_string+, and +content+ properties
     def wait_until_alive(url, headers=header_basic_auth)
+      url = url[1..-1] if url.start_with?("/")
       while !is_alive?("#{@api_url}/#{url}", headers) do
         info("Web server is not ready, waiting...")
         sleep 3
