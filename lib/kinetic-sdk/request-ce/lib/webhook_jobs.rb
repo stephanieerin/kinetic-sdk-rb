12dd49dd812cf75aad2c2f6b48a84ef247e09f1e
homejames203 <james.davies@kineticdata.com>
2018-06-06 18:31:46 -0400

    Added method for fetching all jobs if page token returned

--- lib/kinetic-sdk/request-ce/lib/webhook_jobs.rb
+++ lib/kinetic-sdk/request-ce/lib/webhook_jobs.rb
@@ -11,6 +11,37 @@ module KineticSdk
       get("#{@api_url}/webhookJobs", params, headers)
     end
 
+    # Find all Webhook Jobs for a space.
+    #
+    # This method will process pages of jobs and internally
+    # concatenate the results into a single array.
+    #
+    # Warning - using this method can cause out of memory errors on large
+    #           result sets.
+    #
+    #
+    # @param params [Hash] Query parameters that are added to the URL, such as +status+
+    # @param headers [Hash] hash of headers to send, default is basic authentication and JSON content type
+    # @return [KineticSdk::Utils::KineticHttpResponse] object, with +code+, +message+, +content_string+, and +content+ properties
+    def find_all_space_webhook_jobs(params={}, headers=default_headers)
+      # Make the initial request of pages jobs
+      response = find_space_webhook_jobs(params, headers)
+      # Build Submissions Array
+      jobs = response.content["webhookJobs"]
+      # if a next page token exists, keep retrieving jobs and add them to the results
+      while (!response.content["nextPageToken"].nil?)
+        params['pageToken'] = response.content["nextPageToken"]
+        response = find_space_webhook_jobs(params, headers)
+        # concat the jobs
+        jobs.concat(response.content["webhookJobs"] || [])
+      end
+      final_content = {"webhookJobs" => jobs, "nextPageToken" => nil }
+      # Return the results
+      response.content=final_content
+      response.content_string=final_content.to_json
+      response
+    end
+
     # Find kapp webhook jobs
     #
     # @param kapp_slug [String] the Kapp slug
@@ -20,6 +51,37 @@ module KineticSdk
     def find_kapp_webhook_jobs(kapp_slug, params={}, headers=default_headers)
       info("Finding webhook jobs in the \"#{kapp_slug}\" Kapp")
       get("#{@api_url}/kapps/#{kapp_slug}/webhookJobs", params, headers)
+    end
+
+    # Find all Webhook Jobs for a kapp.
+    #
+    # This method will process pages of jobs and internally
+    # concatenate the results into a single array.
+    #
+    # Warning - using this method can cause out of memory errors on large
+    #           result sets.
+    #
+    #
+    # @param params [Hash] Query parameters that are added to the URL, such as +status+
+    # @param headers [Hash] hash of headers to send, default is basic authentication and JSON content type
+    # @return [KineticSdk::Utils::KineticHttpResponse] object, with +code+, +message+, +content_string+, and +content+ properties
+    def find_all_kapp_webhook_jobs(kapp_slug, params={}, headers=default_headers)
+      # Make the initial request of pages jobs
+      response = find_kapp_webhook_jobs(kapp_slug, params, headers)
+      # Build Submissions Array
+      jobs = response.content["webhookJobs"]
+      # if a next page token exists, keep retrieving jobs and add them to the results
+      while (!response.content["nextPageToken"].nil?)
+        params['pageToken'] = response.content["nextPageToken"]
+        response = find_kapp_webhook_jobs(kapp_slug, params, headers)
+        # concat the jobs
+        jobs.concat(response.content["webhookJobs"] || [])
+      end
+      final_content = {"webhookJobs" => jobs, "nextPageToken" => nil }
+      # Return the results
+      response.content=final_content
+      response.content_string=final_content.to_json
+      response
     end
 
     # Update space webhook job
