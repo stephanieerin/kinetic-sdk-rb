d3c543cc2c76e6dbffa60644341933a1ee185588
Jack Boespflug <jack.boespflug@kineticdata.com>
2018-06-29 12:14:23 -0500

    Update random generator with constants

--- lib/kinetic-sdk/utils/random.rb
+++ lib/kinetic-sdk/utils/random.rb
@@ -5,16 +5,20 @@ module KineticSdk
     # complexity.
     module Random
 
+      DEFAULT_SYMBOLS = %w( ! @ % * ( ) { } [ ] )
+      SEVEN_BIT_ASCII_SYMBOLS = %w(! " # $ % & ' ( ) * + - . / : ; < = > ? @ [ \ ] & _ ` { | } ~)
+      SEVEN_BIT_ASCII_ALPHANUM = 
+        ('A'..'Z').to_a.concat(
+          ('a'..'z').to_a).concat(
+            ('0'..'9').to_a)
+
       # Generates a simple secret based on ASCII alpha-numeric characters,
       # and a few standard symbol characters.
       #
       # @param size [Fixnum] The length of the generated secret
       # @param allowed_symbols [Array] symbols to allow
-      def self.simple(size = 20, allowed_symbols = [ '!', '@', '%', '*', '(', ')', '{', '}', '[', ']' ])
-        chars = (allowed_symbols || Array.new).concat(
-          ('A'..'Z').to_a).concat(
-            ('a'..'z').to_a).concat(
-              ('0'..'9').to_a)
+      def self.simple(size = 20, allowed_symbols = DEFAULT_SYMBOLS)
+        chars = (allowed_symbols || Array.new).concat(SEVEN_BIT_ASCII_ALPHANUM)
         (0...size).map { chars[rand(chars.size)] }.join
       end
 
